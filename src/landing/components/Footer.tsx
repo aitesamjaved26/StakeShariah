5ed80379aa0357c936f5e9411af64357333c800d
aitesamjaved26 <aitesam.javed16@gmail.com>
2023-11-26 00:15:07 +0500

    hide social media links

--- src/landing/components/Footer.tsx
+++ src/landing/components/Footer.tsx
@@ -48,14 +48,14 @@ const Footer = () => (
       </p>
 
       <div className='flex flex-row md:mt-0 mt-6'>
-        {socialMedia.map((social, index) => (
+        {/* {socialMedia.map((social, index) => (
           <img
             key={social.id}
             src={social.icon}
             alt={social.id}
             className={`w-[21px] h-[21px] object-contain cursor-pointer ${
               index !== socialMedia.length - 1 ? 'mr-2' : 'mr-0'
-            }`}
+            }`} */}
           />
         ))}
       </div>
