866c17f3990f4fd09d7027f7d01911c8f8911b75
aitesamjaved26 <aitesam.javed16@gmail.com>
2023-12-09 17:48:35 +0500

    section above footer

--- src/landing/components/Footer.tsx
+++ src/landing/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => (
         />
       </div>
 
-      <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+      {/* <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
         {footerLinks.map((footerLink) => (
           <div
             key={footerLink.title}
@@ -39,6 +39,7 @@ const Footer = () => (
           </div>
         ))}
       </div>
+      </div> */}
       </div>
 
     <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]'>
